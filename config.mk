862f099819d79fc1f220abe5ecfffd5e5c4223c6
ifiddes <ian.t.fiddes@gmail.com>
2015-10-23 08:52:47 -0700

    updated gorilla config

--- config.mk
+++ config.mk
@@ -4,12 +4,13 @@
 # Tag the commit when running the pipeline.
 ####################################################################################################
 
-GORILLA_VERSION = susie_3
+GORILLA_VERSION = susie_3_1
 mappedOrgs = chimp gorilla orang squirrel_monkey
-GENCODE_VERSION = V22
-TRANS_MAP_VERSION = 2015-05-28
+allOrgs = chimp gorilla orang squirrel_monkey human
+GENCODE_VERSION = V23
+TRANS_MAP_VERSION = 2015-10-06
 CHAINING_VERSION = 2015-08-19
-COMPARATIVE_ANNOTATOR_VERSION = 2015-09-01
+COMPARATIVE_ANNOTATOR_VERSION = 2015-10-12
 
 srcOrg = human
 srcOrgHgDb = hg38
